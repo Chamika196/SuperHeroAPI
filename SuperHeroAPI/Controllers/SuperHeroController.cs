2cf7741d50984e064bcc537da552ccbc40d60ff4
Chamika196 <chamikarathnayaka1996@gmail.com>
2023-11-15 10:33:27 +0530

    Add project files.

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Models;
+using SuperHeroAPI.Services.SuperHeroService;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly ISuperHeroService _superHeroServices;
+
+        public SuperHeroController(ISuperHeroService superHeroService)
+        {
+            _superHeroServices = superHeroService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            
+            return await _superHeroServices.GetAllHeroes();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult <SuperHero>> GetSingleHero(int id)
+        {
+            var result =await _superHeroServices.GetSingleHero(id);
+            if (result is null)
+            {
+                return NotFound("Hero not Found");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            var result =await _superHeroServices.AddHero(hero);
+            
+
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(int id, SuperHero request)
+        {
+            var result = await _superHeroServices.UpdateHero(id, request);
+            if (result is null)
+            {
+                return NotFound("Hero not Found");
+            }
+            
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var result = await _superHeroServices.DeleteHero(id);
+            if(result is null)
+            {
+                return NotFound("Hero not Found");
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server =.;Database=superheroapidb;Trusted_Connection=true;TrustServerCertificate=true;MultipleActiveResultSets=true;");
+
+        }
+        public DbSet<SuperHero> SuperHeros { get; set; }
+    }
+
+   
+}
